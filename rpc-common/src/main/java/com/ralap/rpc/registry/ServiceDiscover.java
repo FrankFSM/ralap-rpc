7bce9a91f4f99065ae58f2a9cdb6612e8a1b3b6e
haojixiang <hao_jixiang@163.com>
2018-10-10 15:45:10 +0800

    服务发现注解完善

--- rpc-common/src/main/java/com/ralap/rpc/registry/ServiceDiscover.java
+++ rpc-common/src/main/java/com/ralap/rpc/registry/ServiceDiscover.java
@@ -13,6 +13,7 @@ import org.apache.zookeeper.ZooKeeper;
 /**
  * 服务发现
  * 为客户端提供服务发现功能
+ *
  * @author: ralap
  * @date: created at 2018/9/5 15:27
  */
@@ -22,12 +23,18 @@ public class ServiceDiscover {
 
     /*注册地址*/
     private String registryAddress;
-
+    /*锁初始化（控制创建连接后才能做后续操作）*/
     private CountDownLatch latch = new CountDownLatch(1);
-
+    /*服务器地址*/
     private volatile List<String> dataList = new ArrayList<>();
 
-
+    /**
+     * 初始化服务发现
+     * 1.与zk监理连接
+     * 2.获取监听服务变化
+     *
+     * @param registryAddress zk地址
+     */
     public ServiceDiscover(String registryAddress) {
         this.registryAddress = registryAddress;
         ZooKeeper zk = connectServer();
@@ -38,7 +45,8 @@ public class ServiceDiscover {
 
 
     /**
-     * 发现服务地址信息
+     * 获取发现服务地址信息
+     * 如果为集群，随机获取
      */
     public String discover() {
         String data = null;
@@ -48,6 +56,7 @@ public class ServiceDiscover {
             if (serverCount == 1) {
                 data = this.dataList.get(0);
             } else {
+                //随机获取
                 data = this.dataList.get(ThreadLocalRandom.current().nextInt(serverCount));
             }
         } else {
@@ -59,13 +68,15 @@ public class ServiceDiscover {
 
 
     /**
-     * 创建zk连接
+     * 建立zk连接
      */
     private ZooKeeper connectServer() {
         ZooKeeper zk = null;
         try {
+            //家里连接
             zk = new ZooKeeper(registryAddress, Constant.REGISTRY_ZK_SESSION_TIMEOUT, even -> {
                 if (even.getState() == KeeperState.SyncConnected) {
+                    //建立成功放行
                     latch.countDown();
                 }
             });
@@ -81,12 +92,15 @@ public class ServiceDiscover {
      */
     private void watchNode(ZooKeeper zk) {
         try {
+            //获取服务节点，监听变化
             List<String> childrenList = zk.getChildren(Constant.REGISTRY_ZK_PATH, even -> {
                 if (even.getType() == EventType.NodeChildrenChanged) {
+                    //重置服务信息
                     watchNode(zk);
                 }
             });
             dataList = new ArrayList<>();
+            //获取服务器地址
             for (String children : childrenList) {
                 byte[] bytes = zk.getData(Constant.REGISTRY_ZK_PATH + "/" + children, false, null);
                 dataList.add(new String(bytes));
